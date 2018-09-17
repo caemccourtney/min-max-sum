23cdf8df3586c1207ad4f6f9254254c510e3c539
Carlos <carlos.edu.mcc@gmail.com>
2018-09-18 00:01:56 +0200

    first commit

---
+++ miniMaxSum.py
@@ -0,0 +1,43 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+
+def quicksort(arr):
+    
+    if len(arr)<=1:
+        return arr
+    
+    smaller,equal,larger=[],[],[]
+    
+    pivot = arr[0]
+    
+    for x in arr:
+        if x<pivot:
+            smaller.append(x)
+        elif x==pivot:
+            equal.append(x)
+        else:
+            larger.append(x)
+            
+    return quicksort(smaller)+equal+quicksort(larger)
+
+
+def miniMaxSum(arr):
+
+    sorted_array = quicksort(arr)
+    
+    min_sum = sum(sorted_array[0:4])
+    max_sum = sum(sorted_array[::-1][0:4])
+
+    print(min_sum, max_sum)
+    
+
+    
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
